aa8ec8bef96887344e733cecf0cefd2f33a29810
Isabela Porto <isamporto@hotmail.com>
2024-09-03 16:48:58 -0300

    Feat: import and place pdf and docx extractor methods into app.py

--- src/app.py
+++ src/app.py
@@ -1,4 +1,6 @@
 import os
+from pdf_extractor import pdf_text_extractor
+from docx_extractor import docx_text_extractor
 
 def main():
   original_documents_path = "data/documents/original"
@@ -6,11 +8,12 @@ def main():
   for filename in os.listdir(original_documents_path):
     current_folder_path = os.path.join(original_documents_path, filename)
     for file in os.listdir(current_folder_path):
+      full_current_file_path = f"{current_folder_path}/{file}"
       if file.endswith('.pdf'):
-        print('pdf')
+        pdf_text_extractor(full_current_file_path)
       elif file.endswith('.docx'):
-        print('docx')
+        docx_text_extractor(full_current_file_path)
       else:
-        print('neither')
+        print(f'File ${file} type not supported')
 
 main()
